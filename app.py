5ed6ecb1b803698eecb51fb105f6ce0a7dc81ab6
greatyyx <bigyyx@gmail.com>
2022-05-20 12:55:23 -0700

    support setting base url and app name

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.wrappers import Response
+from flask import Flask, request
 
 from db import db
 from index import index_bp
@@ -11,6 +13,13 @@ def create_app(config):
     app = Flask(__name__)
     app.config['annotator'] = config
     app.secret_key = config['secret_key']
+
+    # base url
+    base_url = app.config['annotator'].get('base_url', '')
+    app.wsgi_app = DispatcherMiddleware(
+        Response('Not Found', status=404),
+        {base_url: app.wsgi_app}
+    )
     # logger = get_logger(__name__, config)
 
     # db connection
@@ -22,5 +31,9 @@ def create_app(config):
     app.register_blueprint(index_bp)
     app.register_blueprint(user_bp, url_prefix='/user')
     app.register_blueprint(admin_bp, url_prefix='/admin')
+
+    @app.context_processor
+    def inject_context():
+        return dict(APP_NAME=app.config['annotator'].get('title', 'Cluster Annotator'), BASE_URL=request.script_root)
 
     return app
